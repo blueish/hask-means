4a693f61c26afd37996154be0cdc9894783981b7
Sam Chow <samchowa@gmail.com>
2017-11-26 16:55:36 -0800

    switch args of closestMean for currying

--- Main.hs
+++ Main.hs
@@ -21,6 +21,12 @@ import Data.List
   )
 -}
 
+testInput = [
+        [0, 0, 0],
+        [2, 2, 2],
+        [2.1, 2.1, 2.1]
+    ]
+
 
 -- ASSUMPTION: D = 3 for all images
 dimensions = 3
@@ -31,21 +37,27 @@ kmeans k x = do
         rg <- newStdGen
         -- our values are RGB values, so we'll take number pairs
         -- from 0 to 255
-        let val = chunksOf dimensions
+        let initialCentroids = chunksOf dimensions
                 . map (*255)
                 . take (dimensions * k) $ (randoms rg :: [Double])
-        return val
+        print "the initial centroids: "
+        print initialCentroids
         -- assign each object to closest mean
+        let closestMeans = map (indexOfClosestMean initialCentroids) x
+        print "initial closest means: "
+        print closestMeans
         -- update the means
         -- recur until the update of the mean matches the new update
         -- return
+        print "returning..."
+        return closestMeans
 
-indexOfClosestMean :: (Floating f, Ord f) => [ f ] -> [[ f ]] -> Int
-indexOfClosestMean rgb centroids = unbox $ elemIndex minDist distances
+indexOfClosestMean :: (Floating f, Ord f) => [[ f ]] -> [ f ] -> Int
+indexOfClosestMean centroids rgb = unbox $ elemIndex minDist distances
     where distances = map (distance rgb) centroids
           minDist = minimum distances
           unbox (Just x) = x
-          unbox (Nothing) = error "this should never happen"
+          unbox (Nothing) = error "Failed to find the index of an element in its own array."
 
 distance :: Floating f => [ f ] -> [f] -> f
 distance point1 point2 = sqrt . foldl (\acc (a, b) -> acc + ((a - b) ^ 2)) 0 $ zip point1 point2
