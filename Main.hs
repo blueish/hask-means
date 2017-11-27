6033e168403ce6a273bda0aa648eb268ab23c8a1
Sam Chow <samchowa@gmail.com>
2017-11-26 16:36:13 -0800

    add fun for finding closest mean

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import System.Random
 import Data.List.Split
+import Data.List
 
 -- main = do
 --     putStrLn "hello world"
@@ -34,7 +35,17 @@ kmeans k x = do
                 . map (*255)
                 . take (dimensions * k) $ (randoms rg :: [Double])
         return val
-    -- assign each object to closest mean
-    -- update the mean
-    -- recur until the update of the mean matches the new update
-    -- return
+        -- assign each object to closest mean
+        -- update the means
+        -- recur until the update of the mean matches the new update
+        -- return
+
+indexOfClosestMean :: (Floating f, Ord f) => [ f ] -> [[ f ]] -> Int
+indexOfClosestMean rgb centroids = unbox $ elemIndex minDist distances
+    where distances = map (distance rgb) centroids
+          minDist = minimum distances
+          unbox (Just x) = x
+          unbox (Nothing) = error "this should never happen"
+
+distance :: Floating f => [ f ] -> [f] -> f
+distance point1 point2 = sqrt . foldl (\acc (a, b) -> acc + ((a - b) ^ 2)) 0 $ zip point1 point2
