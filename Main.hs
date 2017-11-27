d43a4b81b92b4e00281ca84648a9437adbc23adc
Sam Chow <samchowa@gmail.com>
2017-11-27 14:17:10 -0800

    rename centroid -> mean

--- Main.hs
+++ Main.hs
@@ -8,18 +8,18 @@ import Data.List
 
 -- ASSUMPTION: D = 3 for all images (rgb values are 3 integer values)
 dimensions :: Int
-dimensions = 3
+dimensions = 2
 
 -- each RGBValue is (n = Dimension) ints between 0 and 255
 type RGBValue = [Int]
 -- RGBImageData is an array of RGBValues
 type RGBImageData = [RGBValue]
 
--- each centroid is a list of D double values between 0 and 255
-type Centroid = [Double]
+-- each mean is a list of D double values between 0 and 255
+type Mean = [Double]
 
--- a list with a 1:1 map representing a mapping of each RGBValue to each Centroid
-type CentroidAssignments = [Int]
+-- a list with a 1:1 map representing a mapping of each RGBValue to each Mean
+type MeanAssignments = [Int]
 
 {-
  2 -> [ [0,0], [2.1,2] [2,2]] -> 
@@ -44,64 +44,63 @@ testInput = [
 
 -- kmeans :: Fractional f =>Int -> [[ Int ]] -> ([[ f ]], [ Int ])
 kmeans k dataSet = do
-        -- initialize centroids (using Random)
+        -- initialize means (using Random)
         -- our values are RGB values, so we'll take number pairs from 0 to 255
         rg <- newStdGen
-        let initialCentroids = chunksOf dimensions
+        let initialMeans = chunksOf dimensions
                 . take (dimensions * k)
                 $ (randomRs (0, 255) rg :: [Double])
 
-        print "the initial centroids: "
-        print initialCentroids
+        print "the initial means: "
+        print initialMeans
         -- assign each object to initial closest mean
-        let closestMeans = map (indexOfClosestMean initialCentroids) dataSet
+        let closestMeans = map (indexOfClosestMean initialMeans) dataSet
         print "initial closest means: "
         print closestMeans
 
-        let finalMeans = calculateMeansAndCentroids initialCentroids dataSet
+        let finalMeans = calculateMeansAndMeans initialMeans dataSet
         print "returning..."
         return finalMeans
 
--- calculateMeansAndCentroids centroids dataSet =  takes initial centroids and a dataSet, and recalculates the centroids
--- until an update of the centroids lead to no changes, returning the final centroids and the group assignment values for the dataSet
-calculateMeansAndCentroids :: [Centroid] -> RGBImageData -> ([Centroid], CentroidAssignments)
-calculateMeansAndCentroids centroids dataSet = recalculateCentroidsAndAssignments dataSet True centroids initData
-    where initData = map (indexOfClosestMean centroids) dataSet
-
-    -- recalculateCentroidsAndAssignments :: [[Double]] -> (Bool, [[Double]] [[Int]]) -> ([[Double]], [Int])
-
--- recalculateCentroidsAndAssignments dataSet hasChanged newCentroids assignments keeps updating centroids and recalculating the assignments
--- of the centroids until there were no changes in the assignments
-recalculateCentroidsAndAssignments :: RGBImageData -> Bool -> [Centroid] -> CentroidAssignments -> ([Centroid], CentroidAssignments)
-recalculateCentroidsAndAssignments dataSet False centroids assignments = (centroids, assignments)
-recalculateCentroidsAndAssignments dataSet _     centroids assignments = recalculateCentroidsAndAssignments
-    dataSet wasChanged newCentroids newAssignments
-        where newCentroids = recalculateCentroids assignments dataSet 
-              (wasChanged, newAssignments) = updateAssignmentsFlagged newCentroids dataSet assignments
+-- calculateMeansAndMeans means dataSet =  takes initial means and a dataSet, and recalculates the means
+-- until an update of the means lead to no changes, returning the final means and the group assignment values for the dataSet
+calculateMeansAndMeans :: [Mean] -> RGBImageData -> ([Mean], MeanAssignments)
+calculateMeansAndMeans means dataSet = recalculateMeansAndAssignments dataSet True means initData
+    where initData = map (indexOfClosestMean means) dataSet
 
 
--- takes centroids and a dataset, and gives back whether any changed alongside the new mappings
-updateAssignmentsFlagged :: [Centroid] -> RGBImageData -> CentroidAssignments -> (Bool, CentroidAssignments)
-updateAssignmentsFlagged centroids dataSet oldAssignments = (allEqual oldAssignments newAssignments, newAssignments)
-    where newAssignments = map (indexOfClosestMean centroids) dataSet
+-- recalculateMeansAndAssignments dataSet hasChanged newMeans assignments keeps updating means and recalculating the assignments
+-- of the means until there were no changes in the assignments
+recalculateMeansAndAssignments :: RGBImageData -> Bool -> [Mean] -> MeanAssignments -> ([Mean], MeanAssignments)
+recalculateMeansAndAssignments dataSet False means assignments = (means, assignments)
+recalculateMeansAndAssignments dataSet _     means assignments = recalculateMeansAndAssignments
+    dataSet wasChanged newMeans newAssignments
+        where newMeans = recalculateMeans assignments dataSet 
+              (wasChanged, newAssignments) = updateAssignmentsFlagged newMeans dataSet assignments
 
+
+-- takes means and a dataset, and gives back whether any changed alongside the new mappings
+updateAssignmentsFlagged :: [Mean] -> RGBImageData -> MeanAssignments -> (Bool, MeanAssignments)
+updateAssignmentsFlagged means dataSet oldAssignments = (allEqual oldAssignments newAssignments, newAssignments)
+    where newAssignments = map (indexOfClosestMean means) dataSet
 
 
 
--- given the current centroid assignments and the dataset, calculates the new means
-recalculateCentroids :: CentroidAssignments -> RGBImageData -> [Centroid]
-recalculateCentroids assignments dataSet = 
+
+-- given the current mean assignments and the dataset, calculates the new means
+recalculateMeans :: MeanAssignments -> RGBImageData -> [Mean]
+recalculateMeans assignments dataSet = 
     map calculateNewMean
     . map removeGroupingLabels
     . groupBy (\a b -> (fst a) == (fst b)) 
     $ zip assignments dataSet
 
 
--- returns the index of the closest centroid (mean)
--- used for assignment of the dataset to centroids
-indexOfClosestMean :: [Centroid] -> RGBValue -> Int
-indexOfClosestMean centroids rgb = unbox $ elemIndex minDist distances
-    where distances = map (distance rgb) centroids
+-- returns the index of the closest mean
+-- used for assignment of the dataset to means
+indexOfClosestMean :: [Mean] -> RGBValue -> Int
+indexOfClosestMean means rgb = unbox $ elemIndex minDist distances
+    where distances = map (distance rgb) means
           minDist = minimum distances
           unbox (Just x) = x
           unbox (Nothing) = error "Failed to find the index of an element in its own array."
@@ -112,7 +111,7 @@ removeGroupingLabels :: [(a, b)] -> [b]
 removeGroupingLabels arr = map snd arr
 
 -- average of n points with dimensions d into one point with dimension d
-calculateNewMean :: [RGBValue] -> Centroid
+calculateNewMean :: [RGBValue] -> Mean
 calculateNewMean arr = divVec (foldl addVec initialZeroVector arr) (length arr)
     where initialZeroVector = take dimensions $ repeat 0
 
@@ -130,11 +129,11 @@ allEqual :: Eq a => [a] -> [a] -> Bool
 allEqual c1 c2 = and $ zipWith (==) c1 c2
 
 -- Divides each value of vec by n
-divVec :: [Int] -> Int -> Centroid
+divVec :: [Int] -> Int -> Mean
 divVec vec n = map (\a -> a / (fromIntegral n)) $ map fromIntegral vec
 
 
-distance :: RGBValue -> Centroid -> Double
+distance :: RGBValue -> Mean -> Double
 distance rgb cent = sqrt 
     . foldl (\acc (a, b) -> acc + ((a - b) ^ 2)) 0
     -- have to map each Int so we can add them to doubles
